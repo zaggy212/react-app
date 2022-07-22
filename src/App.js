74bb565e8862cdcf8009492e8ab080d063d3b932
zaggy212 <alagwugalileo212@gmail.com>
2022-07-22 14:39:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import img1 from './images/mk-main1.jpg';
+import img2 from './images/markov-btc.jpg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <div>
+      <img src={img1} id='loading'/>
+    </div> */}
+      <div id='myDiv'>
+        <img src={img1} className='App-logo'/>
+          
+            
+          <div className='text reveal'>
+            {/* <h4 >WHY US?</h4> */}
+            <h2>MARKOV IS  BUILT ON AUTOMATION WHICH GUARANTEES SPEED AS WELL AS CONSTANT DOXXING WHICH GUARANTEES RELIABILITY.</h2>
+          </div>
+            
+          <div className='img-background'>
+            
+          </div>
+      
+      </div>
+    
+          
+      
+    
+    
+    </>
+    
+    
+
+        
   );
 }
 
